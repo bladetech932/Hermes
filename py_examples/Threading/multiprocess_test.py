28a86d2b6d6295a367f56ed26621cb3cb4f3aa09
10475608 <bladetech932@gmail.com>
2020-01-31 17:18:39 -0600

    test commit

--- py_examples/Threading/multiprocess_test.py
+++ py_examples/Threading/multiprocess_test.py
@@ -12,15 +12,12 @@ def count():
 if __name__ == "__main__":
     number1 = 7777777777777777777777777777777
     number2 = 8888888888888888888888888888888
-    number3 = 9999999999999999999999999999999
+
     p1 = multiprocessing.Process(target=count)
     p2 = multiprocessing.Process(target=calc_quad, args=(number2,))
-    p3 = multiprocessing.Process(target=calc_quad, args=(number3,))
 
     p1.start()
     p2.start()
-    p3.start()
 
     p1.join()
     p2.join()
-    p3.join()
