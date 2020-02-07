eb6f29d7f33b7eb77c8e8ed8c0ef6c42a0412e48
J-Fogg <jordan.fogg@okstate.edu>
2020-02-07 04:22:57 -0600

    .

--- py_examples/UART/Uidirectional/uart_bidirectional.py
+++ py_examples/UART/Uidirectional/uart_bidirectional.py
@@ -6,10 +6,11 @@ ser.port = "/dev/ttyS0"
 if (ser.isOpen() == False):
         ser.open()
 while True:
-       
-	output = ser.readline() # Receives the same number of bytes as the input is long
+        Input = input("Enter Input: ")
+        Input1 = (Input + "\n")
+        ser.write (Input1.encode()) # The .encode method encodes the Input into bytes
+        output = ser.readline() # Receives the same number of bytes as the input is long
         output1 = output.decode()
         print (output1)
 ser.close()
-
 
